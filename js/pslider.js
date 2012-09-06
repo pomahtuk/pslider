aadebfd858ea60bdf7ba624ea7e28681e3accf17
Ilja Lovrjakov <pman89@yandex.ru>
2012-09-06 16:10:01 +0400

    Fast switch bug fix

--- js/pslider.js
+++ js/pslider.js
@@ -27,40 +27,49 @@ pSlider, a very simple image slider by pman
 			container.css({'width':option.width*2});
 			//устанавливаем размер панели в 2 ширины слайдера
 			
+			var animating = false;//флаг активной анимации
+			
 			if (total >= 2) {//если есть, что переключать
 				$(this).append('<a href="#" class="next"></a>'); //добавляем кнопки переключения слайдов
 				$(this).prepend('<a href="#" class="prev"></a>'); 
 				container.append('<img src="'+$(items.get(0)).children("img").attr("src")+'">');//и первичный слайд
 							
 				$('.pSlider a.next').click(function(){ //добавляем обработчик для кнопки "Далее"
-					current = current === total-1? 0: current+1; //вычисляем изображение для показа
-					$('.pSliderPane img.old').remove();//убираем старое изображение	на всякий случай
-					container.children('img').addClass('old'); //помечаем старое изображение классом old
-					container.append('<img src="'+$(items.get(current)).children("img").attr("src")+'">');//добавляем следующее на панель после старого
-					$('.pSliderPane').css({'margin-left':'0'});//на всякий случай сбрасываем положение панели
-					$('.pSliderPane').stop(false,false).animate({ //анимированно сдвигаем панель на ширину слайдера влево, тем самым уводя из обзора старую картинку и показывая новую
-						'margin-left': '-='+option.width 
-					}, 1000, function() {//после анимации, длиной в секунду
-						$('.pSliderPane img.old').remove(); //удаляем старое изображение
-						container.css({'margin-left':'0'}); //возвращаем панель на место
-						//animating = false;//закончили анимацию, всё в номре.
-					});	
+					if (!animating) {//Если нет активной анимации - работаем, в противном случае игнорируем клик.
+						animating = true;//начинаем анимацию
+						current = current === total-1? 0: current+1; //вычисляем изображение для показа
+						$('.pSliderPane img.old').remove();//убираем старое изображение	на всякий случай
+						container.children('img').addClass('old'); //помечаем старое изображение классом old
+						container.append('<img src="'+$(items.get(current)).children("img").attr("src")+'">');//добавляем следующее на панель после старого
+						$('.pSliderPane').css({'margin-left':'0'});//на всякий случай сбрасываем положение панели
+						$('.pSliderPane').animate({ //анимированно сдвигаем панель на ширину слайдера влево, тем самым уводя из обзора старую картинку и показывая новую
+							'margin-left': '-='+option.width 
+						}, 1000, function() {//после анимации, длиной в секунду
+							$('.pSliderPane img.old').remove(); //удаляем старое изображение
+							container.css({'margin-left':'0'}); //возвращаем панель на место
+							animating = false;//закончили анимацию, всё в номре.
+						});
+					};
 					
 					return false; //предотвращаем дефолтное поведение ссылки
 				})
 				
 				$('.pSlider a.prev').click(function(){ //обработчик события "Назад"
-					current = current === 0? total-1: current-1; //вычисляем, какой изображение показывать
-					$('.pSliderPane img.old').remove();//убираем старое изображение	на всякий случай										
-					container.children('img').addClass('old'); //маркируем старое изображение
-					container.prepend('<img src="'+$(items.get(current)).children("img").attr("src")+'" >');//добавляем новое до старого
-					$('.pSliderPane').css({'margin-left':-option.width});//сдвигаем панель на исходную
-					$('.pSliderPane').stop(false,false).animate({//двигаем до нуля
-						'margin-left': '+='+option.width
-					}, 1000, function() {//а после секундной анимации
-						$('.pSliderPane img.old').remove();//убираем старое изображение
-						//container.css({'margin-left':'0'});
-					});
+					if (!animating) {//Если нет активной анимации - работаем, в противном случае игнорируем клик.
+						animating = true;//начинаем анимацию
+						current = current === 0? total-1: current-1; //вычисляем, какой изображение показывать
+						$('.pSliderPane img.old').remove();//убираем старое изображение	на всякий случай										
+						container.children('img').addClass('old'); //маркируем старое изображение
+						container.prepend('<img src="'+$(items.get(current)).children("img").attr("src")+'" >');//добавляем новое до старого
+						$('.pSliderPane').css({'margin-left':-option.width});//сдвигаем панель на исходную
+						$('.pSliderPane').animate({//двигаем до нуля
+							'margin-left': '+='+option.width
+						}, 1000, function() {//а после секундной анимации
+							$('.pSliderPane img.old').remove();//убираем старое изображение
+							//container.css({'margin-left':'0'});
+							animating = false;//закончили анимацию, всё в номре.
+						});
+					};
 					
 					return false; //предотвращаем стандартное поведение ссылки
 				})				
